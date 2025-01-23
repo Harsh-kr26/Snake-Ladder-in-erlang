28c0e774baa2c7760281ac4bd0b876f8e809f04e
Harsh Kumar <harsh.k@greyorange.com>
2025-01-23 16:05:14 +0530

    erlang code for snake and ladder

--- code.erl
+++ code.erl
@@ -1,14 +1,30 @@
--module(snake_ladder).
+-module(snake).
 -export([min_throw/2]).
 
-min_throw(N,arr) ->
+%N = 8.
+%Arr = [3,22,5,8,11,26,20,29,17,4,19,7,27,1,21,9].
 
-    Moves = create_map(N,arr),
+min_throw(N, Arr) ->
+    Moves = create_moves(N, Arr),
+    Visited = maps:from_list([{i, false} || i <- lists:seq(1, 30)]),
+    Queue = [{1, 0}],
+    bfs(Queue, Visited, Moves).
 
-    Visited = maps:from_list([{i,false} || i <- lists:seq(1,100)]),
+bfs([{Node, PrevMoves} | RemainingQueue], Visited, Moves) when Node =:= 30 -> 
+    PrevMoves;
+bfs([{Node, PrevMoves} | RemainingQueue], Visited, Moves) ->
+    NextPositions = lists:filter(fun(Pos) -> Pos =< 30 andalso maps:get(Pos, Visited) =:= false end,
+       lists:seq(Node + 1, min(Node + 6, 30))),
+    NewVisited = lists:foldl(fun(Pos, Acc) -> maps:put(Pos, true, Acc) end, Visited, NextPositions),
+    NewQueue = RemainingQueue ++
+        lists:map(fun(Pos) ->
+            {maps:get(Pos, Moves), PrevMoves + 1}
+        end, NextPositions),
+    bfs(NewQueue, NewVisited, Moves);
 
-    Q = queue:in([1,0],Q).
+bfs(_, _, _) -> infinity.
 
-    bfs(Q, Visited, Moves).
-
-% To Do : create bfs and create_map function
+create_moves(N, Arr) ->
+    lists:foldl(fun(I, Acc) ->
+               maps:put(lists:nth(I, Arr), lists:nth(I + 1, Arr), Acc)
+           end, #{}, lists:seq(1, 2*N, 2)).
